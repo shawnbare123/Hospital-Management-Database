d5e7732f4187bf597b0f26235c5e474ec30a367a
Shawn <shawnbare2021@gmail.com>
2025-09-17 14:09:48 -0500

    Updated Entity Names

--- Database Initialization.sql
+++ Database Initialization.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE IF NOT EXISTS hospital_db;
 USE hospital_db;
 
-DROP TABLE IF EXISTS Patients;
-DROP TABLE IF EXISTS Doctors;
-DROP TABLE IF EXISTS Medical_Records;
+DROP TABLE IF EXISTS Patient;
+DROP TABLE IF EXISTS Doctor;
+DROP TABLE IF EXISTS Medical_Record;
 DROP TABLE IF EXISTS Billing;
 
-CREATE TABLE Patients (
+CREATE TABLE Patient (
 	MRN SMALLINT UNSIGNED PRIMARY KEY,
     First_Name VARCHAR(20),
     Last_Name VARCHAR(20),
     Birthdate DATE
 );
 
-CREATE TABLE Doctors (
+CREATE TABLE Doctor (
 	Employee_ID SMALLINT UNSIGNED PRIMARY KEY,
     First_Name VARCHAR(20),
     Last_Name VARCHAR(20),
     Specialty VARCHAR(30)
 );
 
-CREATE TABLE Medical_Records (
+CREATE TABLE Medical_Record (
 	Record_ID SMALLINT UNSIGNED PRIMARY KEY,
     Patient_MRN SMALLINT UNSIGNED,
     Doctor_ID SMALLINT UNSIGNED,
